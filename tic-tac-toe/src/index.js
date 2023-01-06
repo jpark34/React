c2fd6765a9a764dde2dd88c929ca7ccc40d66d6f
Jarrett Parker <jarrettparker34@gmail.com>
2023-01-05 21:39:49 -0500

    Gave the state of the square to the board

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -2,28 +2,52 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+// each square is a controlled component as the board class contains
+// the states and values for each one and has full control over them
 class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-
     render() {
         return (
-            <button className="square"
-            onClick={() => this.setState({value: 'X'})}
+            <button 
+                className="square"
+                onClick={() => this.props.onClick()}
             >
-                {this.state.value}
+                {this.props.value}
             </button>
         );
     }
 }
-  
+
+// extends allows for the class to inherit methods from another class
 class Board extends React.Component {
+    // constructor allows you to create an instance of the object
+    constructor(props) {
+        // super allows children methods to gain access
+        // to their parent's properties and methods
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+
+    // convention to use handle[Event] for methods that handle events
+    handleClick(i) {
+        // slice creates a copy of the squares array so that
+        // we are not modifying the existing array. This allows for immutability and
+        // allows for us to have a pure component, where we can easily determine if
+        // a change has been made to the object
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
     renderSquare(i) {
-        return <Square value={i} />;
+        return (
+            <Square 
+                value={this.state.squares[i]}
+                // convention to use on[Event] names for props that represent events
+                onClick={() => this.handleClick(i)}
+            />
+        );
     }
   
     render() {
