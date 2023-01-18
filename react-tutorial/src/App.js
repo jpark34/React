7006d6eeb232658a20465a33faeb183f7de66473
Jarrett Parker <jarrettparker34@gmail.com>
2023-01-17 21:39:03 -0500

    Added ability to change state of the table

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -4,8 +4,9 @@ import Table from './Table'
 // creates the app component
 // each component should get their own file
 class App extends Component {
-    render() {
-        const characters = [
+    // create a state object to allow 2 way data flow
+    state = {
+        characters: [
             {
                 name: 'Charlie',
                 job: 'Janitor',
@@ -22,15 +23,57 @@ class App extends Component {
                 name: 'Dennis',
                 job: 'Bartender',
             },
-        ]
+        ],
+    }
 
-        // pass the json like array to the table class as a prop
+    // pass the state and removeCharacter function to the table class
+    render() {
+        const { characters } = this.state
+        
         return (
-            <div className="container">
-                <Table characterData={characters} />
+            <div className='container'>
+                <Table characterData={characters} removeCharacter={this.removeCharacter} />
             </div>
         )
     }
+
+    removeCharacter = (index) => {
+        const { characters } = this.state
+    
+        this.setState({
+            characters: characters.filter((character, i) => {
+                return i !== index
+            })
+        })
+    }
+
+    // render() {
+    //     const characters = [
+    //         {
+    //             name: 'Charlie',
+    //             job: 'Janitor',
+    //         },
+    //         {
+    //             name: 'Mac',
+    //             job: 'Bouncer',
+    //         },
+    //         {
+    //             name: 'Dee',
+    //             job: 'Aspiring Actress',
+    //         },
+    //         {
+    //             name: 'Dennis',
+    //             job: 'Bartender',
+    //         },
+    //     ]
+
+    //     // pass the json like array to the table class as a prop
+    //     return (
+    //         <div className="container">
+    //             <Table characterData={characters} />
+    //         </div>
+    //     )
+    // }
 }
 
 export default App
--- react-tutorial/src/Table.js
+++ react-tutorial/src/Table.js
@@ -4,17 +4,28 @@ import React, { Component } from "react"
 // capitalize these in order to differentiate them
 // from regular HTML elements
 // a class component must include render()
-class Table extends Component {
-    render() {
-        const { characterData } = this.props
+// class Table extends Component {
+//     render() {
+//         const { characterData } = this.props
 
-        return (
-            <table>
-                <TableHeader />
-                <TableBody characterData={characterData} />
-            </table>
-        )
-    }
+//         return (
+//             <table>
+//                 <TableHeader />
+//                 <TableBody characterData={characterData} />
+//             </table>
+//         )
+//     }
+// }
+
+const Table = (props) => {
+    const { characterData, removeCharacter } = props
+
+    return (
+        <table>
+            <TableHeader />
+            <TableBody characterData={characterData} removeCharacter={removeCharacter} />
+        </table>
+    )
 }
 
 // simple component
@@ -36,6 +47,9 @@ const TableBody = (props) => {
             <tr key={index}>
                 <td>{row.name}</td>
                 <td>{row.job}</td>
+                <td>
+                    <button onClick={() => props.removeCharacter(index)}>Delete</button>
+                </td>
             </tr>
         )
     })
