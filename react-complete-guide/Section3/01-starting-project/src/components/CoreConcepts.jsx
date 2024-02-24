b42922d122cda9f9e89f51775f990c0488226ebf
Jarrett Parker <jarrettparker34@gmail.com>
2024-02-23 22:32:17 -0500

    Created components folder as best practice

---
+++ react-complete-guide/Section3/01-starting-project/src/components/CoreConcepts.jsx
@@ -0,0 +1,21 @@
+// one way to send props
+// function CoreConcept(props) {
+//   return (
+//     <li>
+//       <img src={props.image} alt={props.title} />
+//       <h3>{props.title}</h3>
+//       <p>{props.description}</p>
+//     </li>
+//   )
+// }
+
+// object desctructuring way of sending props
+export default function CoreConcept({image, title, description}) {
+    return (
+      <li>
+        <img src={image} alt={title} />
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </li>
+    )
+  }
