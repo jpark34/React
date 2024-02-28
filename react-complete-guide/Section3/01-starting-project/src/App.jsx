ddf4cf29699db402a6f21e64b1623b75e806ba4e
Jarrett Parker <jarrettparker34@gmail.com>
2024-02-27 22:24:45 -0500

    Cleaned up file structure and used the children prop

--- react-complete-guide/Section3/01-starting-project/src/App.jsx
+++ react-complete-guide/Section3/01-starting-project/src/App.jsx
@@ -1,6 +1,7 @@
 import { CORE_CONCEPTS } from './data.js';
-import Header from './components/Header.jsx';
+import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcepts.jsx';
+import TabButton from './components/TabButton.jsx';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <Header></Header>
       <main>
         <section id="core-concepts">
-          <h2>Time to get started!</h2>
+          <h2>Core Concepts</h2>
           <ul>
             <CoreConcept 
               title={CORE_CONCEPTS[0].title}
@@ -19,6 +20,15 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
